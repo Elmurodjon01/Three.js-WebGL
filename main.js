245d54965aeead20676e46194a57d8a9465f167e
Elmurodjon <96878908+Elmurodjon01@users.noreply.github.com>
2024-10-01 21:56:09 +0900

    process: axesHelper

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
 const cubeGeometry = new THREE.BoxGeometry(1,1,1);
-const cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
+const cubeMaterial = new THREE.MeshBasicMaterial({color: 'grey'});
 
 const cubeMesh = new THREE.Mesh(
   cubeGeometry,
@@ -15,10 +16,23 @@ scene.add(cubeMesh);
 //initialize camera 
 const camera = new THREE.PerspectiveCamera(
   75, 
-  innerWidth / innerHeight,
+  window.innerWidth / window.innerHeight,
   0.1, //this is camera distance and anything closer than this, user won't be able to see
   30, //anything further than this, user can not also see
 );
+
+
+//orthographic camera
+
+// const aspectRatio = window.innerWidth / window.innerHeight;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio, 
+//   1 * aspectRatio, 
+//   1, 
+//   -1, 
+//   0.1, 
+//   200
+// );
 
 //Anything created in the scene will be at point 0 by default, camera, mesh or any other 3D objects. it is like camera being inside mesh or 3D object, where it is too hard to see anything as camera is inside that thing that has to be seen
 //so lets move the camera back
@@ -29,11 +43,32 @@ scene.add(camera);
 const canvas = document.querySelector('canvas.threejs');
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
+  antialias: true, //it removes stair case patterns on the edge of 3D object
 });
+
+
+
+//initialize controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
 //initialize renderer
 
+renderer.setPixelRatio(window.devicePixelRatio, 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
 
-console.log(canvas);
+const renderLoop = ()=> {
+controls.update();
+renderer.render(scene, camera);
+window.requestAnimationFrame(renderLoop)
+}
+
+
+window.addEventListener(('resize'), ()=> {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+renderer.setSize(window.innerWidth, window.innerHeight);
+})
+
+renderLoop();
